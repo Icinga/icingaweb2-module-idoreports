bdc8c14764b4099a613501535a85b242da110f2c
Eric Lippmann <eric.lippmann@icinga.com>
2019-04-25 12:54:58 +0200

    DB: Remove timestamp defaults for NOT NULL
    fixes #1

--- schema/slaperiods.sql
+++ schema/slaperiods.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS icinga_sla_periods;
 CREATE TABLE icinga_sla_periods (
   timeperiod_object_id BIGINT(20) UNSIGNED NOT NULL,
-  start_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  start_time timestamp NOT NULL,
   end_time timestamp NULL DEFAULT NULL,
   PRIMARY KEY tp_start (timeperiod_object_id, start_time),
   UNIQUE KEY tp_end (timeperiod_object_id, end_time)
@@ -10,7 +10,7 @@ CREATE TABLE icinga_sla_periods (
 DROP TABLE IF EXISTS icinga_outofsla_periods;
 CREATE TABLE icinga_outofsla_periods (
   timeperiod_object_id BIGINT(20) UNSIGNED NOT NULL,
-  start_time timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  start_time timestamp NOT NULL,
   end_time timestamp NULL DEFAULT NULL,
   PRIMARY KEY tp_start (timeperiod_object_id, start_time),
   UNIQUE KEY tp_end (timeperiod_object_id, end_time)
