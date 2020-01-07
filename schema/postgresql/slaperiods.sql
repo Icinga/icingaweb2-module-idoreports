bd717f0131ec06c394b883fcd3d64f247ad1d14e
Gunnar Nick Bluth <gunnar.bluth@pro-open.de>
2020-01-07 11:25:16 +0100

    Support PostgreSQL
    Signed-off-by: Eric Lippmann <eric.lippmann@icinga.com>

---
+++ schema/postgresql/slaperiods.sql
@@ -0,0 +1,5 @@
+CREATE TABLE icinga_outofsla_periods (
+    timeperiod_object_id numeric NOT NULL,
+    start_time timestamp with time zone NOT NULL,
+    end_time timestamp with time zone NOT NULL
+);
