934fab98bfddcf9282a5104ce781ff1a5b824a99
Gunnar "Nick" Bluth <gunnar.bluth@pro-open.de>
2020-02-10 18:46:05 +0100

    use latest, working version of PostgreSQL function

--- schema/postgresql/get_sla_ok_percent.sql
+++ schema/postgresql/get_sla_ok_percent.sql
@@ -1,210 +1,173 @@
 CREATE OR REPLACE FUNCTION idoreports_get_sla_ok_percent(
-    id bigint,
-    starttime timestamp with time zone,
-    endtime timestamp with time zone,
-    sla_id integer DEFAULT NULL
-)
-    RETURNS float
-    LANGUAGE SQL
+	id INTEGER,
+	starttime TIMESTAMP WITH TIME ZONE,
+	endtime  TIMESTAMP WITH TIME ZONE,
+	sla_id INTEGER DEFAULT NULL
+) 
+RETURNS float
+LANGUAGE SQL
 AS $$
 --\set id 371
 --\set starttime '2019-02-19 00:00:00'
 --\set endtime '2019-02-20 10:00:00'
---\set sla_id null
-WITH
-    before AS (
-        -- low border, last event before the range we are looking for:
-        SELECT
-            down,
-            state_time,
-            state
-        FROM (
-            (
-                SELECT
-                    1 AS prio,
-                   state > 1 AS down,
-                   GREATEST(state_time, starttime) AS state_time,
-                   state
-                FROM
-                    icinga_statehistory
-                WHERE
-                    object_id = id
-                    AND state_time < starttime
-                    AND state_type = 1
-                ORDER BY
-                    state_time DESC
-                LIMIT 1
-            )
-            UNION ALL
-            (
-                SELECT
-                    2 AS prio,
-                    state > 1 AS down,
-                    GREATEST(state_time, starttime) AS state_time,
-                    state
-                FROM
-                    icinga_statehistory
-                WHERE
-                    object_id = id
-                    AND state_time < starttime
-                ORDER BY
-                    state_time DESC
-                LIMIT 1
-            )
-        ) ranked
-        ORDER BY
-            prio
-        LIMIT 1
-    ),
+--\set sla_id null 
 
-    all_hard_events AS (
-        -- the actual range we're looking for:
-        SELECT
-            state > 1 AS down,
-            state_time,
-            state
-        FROM
-            icinga_statehistory
-        WHERE
-            object_id = id
-            AND state_time >= starttime
-            AND state_time <= endtime
-            AND state_type = 1
-    ),
+WITH before AS (
+	-- low border, last event before the range we are looking for:
+	SELECT down, state_time_ AS state_time,state FROM (
+	(SELECT 1 AS prio
+		,state > 1 AS down
+		,GREATEST(state_time,starttime) AS state_time_
+		,state
+	FROM icinga_statehistory 
+	WHERE 
+		object_id = id
+	   AND	state_time < starttime
+	   AND  state_type = 1
+	ORDER BY state_time DESC
+	LIMIT 1)
+	UNION ALL
+	(SELECT 2 AS prio
+		,state > 1 AS down
+		,GREATEST(state_time,starttime) AS state_time_
+		,state
+	FROM icinga_statehistory 
+	WHERE 
+		object_id = id
+	   AND	state_time < starttime
+	ORDER BY state_time DESC
+	LIMIT 1)
 
-    after AS (
-        -- the "younger" of the current host/service state and the first recorded event
-        (
-            SELECT
-                state > 1 AS down,
-                LEAST(state_time, endtime) AS state_time,
-                state
-            FROM (
-                (
-                    SELECT
-                        state_time,
-                        state
-                    FROM
-                        icinga_statehistory
-                    WHERE
-                        object_id = id
-                        AND state_time > endtime
-                        AND state_type = 1
-                    ORDER BY
-                        state_time
-                    LIMIT 1
-                )
-                UNION ALL
-                (
-                    SELECT
-                        status_update_time,
-                        current_state
-                    FROM
-                        icinga_hoststatus
-                    WHERE
-                        host_object_id = id
-                        AND state_type = 1
-                )
-                UNION ALL
-                (
-                    SELECT
-                        status_update_time,
-                        current_state
-                    FROM
-                        icinga_servicestatus
-                    WHERE
-                        service_object_id = id
-                        AND state_type = 1
-                )
-              ) AS after_searched_period
-            ORDER BY
-                state_time
-            LIMIT 1
-        )
-    ),
+	) ranked ORDER BY prio 
+	LIMIT 1
+),
+all_hard_events AS (
+	-- the actual range we're looking for:
+	SELECT state > 1 AS down
+		,state_time
+		,state
+	FROM icinga_statehistory 
+	WHERE 
+		object_id = id
+	AND	state_time >= starttime
+	AND 	state_time <= endtime
+	AND 	state_type = 1
+),
 
-    allevents AS (
-        TABLE before
-        UNION ALL
-        TABLE all_hard_events
-        UNION ALL
-        TABLE after
-    ),
+after AS (
+	-- the "younger" of the current host/service state and the first recorded event
+	(SELECT state > 1 AS down
+		,LEAST(state_time,endtime) AS state_time
+		,state
+		
+		 FROM (
+		(SELECT state_time
+			,state
+		FROM icinga_statehistory 
+		WHERE 
+			object_id = id
+			AND	state_time > endtime
+		AND     state_type = 1
+		ORDER BY state_time ASC
+		LIMIT 1)
 
-    downtimes AS (
-        (
-            SELECT
-                tstzrange(
---                     GREATEST(actual_start_time, starttime), LEAST(actual_end_time, endtime)
-                    actual_start_time, actual_end_time
-                ) AS downtime
-            FROM
-                icinga_downtimehistory
-            WHERE
-              object_id = id
---               AND actual_start_time <= endtime
---               AND COALESCE(actual_end_time, starttime) >= starttime
-        )
-        UNION ALL
-        (
-            SELECT
-                tstzrange(
---                     GREATEST(start_time, starttime), LEAST(end_time, endtime)
-                    start_time, end_time
-                ) AS downtime
-            FROM
-                icinga_outofsla_periods
-            WHERE
-                timeperiod_object_id = sla_id
-        )
-    ),
+		UNION ALL
 
-    relevant AS (
-        --SELECT * FROM allevents;
-        SELECT
-            down,
-            tstzrange(state_time, COALESCE(lead(state_time) OVER w, endtime),'(]') AS timerange
---             ,lead(state_time) OVER w - state_time AS duration
-        FROM (
-            SELECT
-                state > 1 AS down,
-                lag(state) OVER w > 1 AS next_down,
-                state_time,
-                state
-            FROM
-                allevents
-            WINDOW
-                w AS (ORDER BY state_time)
-         ) alle
-        WHERE
-            down != next_down
-        WINDOW
-            w AS (ORDER BY state_time)
-    ),
+		SELECT status_update_time
+			,current_state
+		FROM icinga_hoststatus
+		WHERE host_object_id = id
+		AND     state_type = 1
 
-    relevant_down AS (
-        SELECT
-            *,
-            timerange * downtime AS covered,
-            COALESCE(timerange - downtime, timerange) AS not_covered
-        FROM
-            relevant
-        LEFT JOIN downtimes ON timerange && downtime
-        WHERE
-            down
-    ),
+		UNION ALL
 
-    effective_downtimes AS (
-        SELECT
-            not_covered,
-            upper(not_covered) - lower(not_covered) AS duration
-        FROM
-            relevant_down
-    )
+		SELECT status_update_time
+			,current_state
+		FROM icinga_servicestatus
+		WHERE service_object_id = id
+		AND   state_type = 1
+	) AS after_searched_period 
+	ORDER BY state_time ASC LIMIT 1)
+)
+, allevents AS (
+	TABLE before 
+	UNION ALL
+	TABLE all_hard_events
+	UNION ALL
+	TABLE after
+)
+, downtimes AS (
+	SELECT tstzrange(
+			--GREATEST(actual_start_time, starttime)
+			--, LEAST(actual_end_time, endtime)
+			actual_start_time
+		      , actual_end_time
+		) AS downtime
+	FROM icinga_downtimehistory
+        WHERE object_id = id
+	--          AND actual_start_time <= endtime
+--          AND COALESCE(actual_end_time,starttime) >= starttime
 
---SELECT * FROM effective_downtimes;
-SELECT
-    100.0 - EXTRACT('epoch' FROM SUM(duration)) / EXTRACT('epoch' FROM endtime - starttime ) * 100.0 AS availability
-FROM
-    effective_downtimes;
+	UNION ALL
+
+	SELECT tstzrange(
+			--GREATEST(start_time, starttime)
+			--, LEAST(end_time, endtime)
+			start_time
+		      , end_time
+		) AS downtime
+	FROM icinga_outofsla_periods
+        WHERE timeperiod_object_id = sla_id
+
+)
+
+--SELECT * FROM allevents;
+, relevant AS (
+	SELECT down
+	,tstzrange(state_time, COALESCE(lead(state_time) OVER w, endtime),'(]') AS zeitraum
+		--,lead(state_time) OVER w - state_time AS dauer
+	FROM (
+		SELECT state > 1 AS down
+		       , lead(state,1,state) OVER w > 1 AS next_down
+		       , lag(state,1,state) OVER w > 1 AS prev_down
+		       , state_time
+		       , state
+		FROM allevents 
+		WINDOW w AS (ORDER BY state_time)
+	) alle
+	WHERE down != next_down OR down != prev_down
+	WINDOW w AS (ORDER BY state_time)
+)
+, relevant_down AS (
+	SELECT *
+		,zeitraum * downtime AS covered 
+		,COALESCE(
+			zeitraum - downtime
+		       ,zeitraum
+		) AS not_covered
+	FROM relevant
+	LEFT JOIN downtimes 
+	  ON zeitraum && downtime
+	WHERE down
+) 
+, effective_downtimes AS (
+	SELECT not_covered
+		, upper(not_covered) - lower(not_covered) AS dauer
+	FROM relevant_down
+)
+
+--select * from effective_downtimes;
+
+, final_result AS (
+	SELECT sum(dauer) AS total_downtime
+		, endtime - starttime AS considered
+		, COALESCE(extract ('epoch' from sum(dauer)),0) AS down_secs
+		, extract ('epoch' from endtime - starttime ) AS considered_secs
+	FROM effective_downtimes
+)
+
+SELECT -- *,
+ 100.0 - down_secs / considered_secs * 100.0 AS availability
+FROM final_result
+;
 $$;
