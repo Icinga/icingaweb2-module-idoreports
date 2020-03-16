776880f9adda73c3f45d9ce379785457e6177c5a
Michael Banck <mbanck@debian.org>
2020-03-16 20:49:33 +0100

    Additional testcase

--- schema/postgresql/t/05-fill-icinga_objects.sql
+++ schema/postgresql/t/05-fill-icinga_objects.sql
@@ -1,4 +1,5 @@
 SELECT plan(1);
 INSERT INTO icinga_objects VALUES (1,1);
 INSERT INTO icinga_objects VALUES (2,2);
-SELECT is(count(*), 2::bigint, 'icinga_objects has 2 rows') FROM icinga_objects;
+INSERT INTO icinga_objects VALUES (3,1);
+SELECT is(count(*), 3::bigint, 'icinga_objects has 3 rows') FROM icinga_objects;
