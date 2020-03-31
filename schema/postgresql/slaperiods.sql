a4b78aedcfe97809b3650a156079b9f45cb17021
Eric Lippmann <eric.lippmann@icinga.com>
2020-03-31 17:01:03 +0200

    Use timestamp w/o time zone in slaperiods as well

--- schema/postgresql/slaperiods.sql
+++ schema/postgresql/slaperiods.sql
@@ -1,5 +1,5 @@
 CREATE TABLE icinga_outofsla_periods (
     timeperiod_object_id numeric NOT NULL,
-    start_time timestamp with time zone NOT NULL,
-    end_time timestamp with time zone NOT NULL
+    start_time timestamp without time zone NOT NULL,
+    end_time timestamp without time zone NOT NULL
 );
