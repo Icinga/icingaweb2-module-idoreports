f93ff0785e9a33d71bbb9bc0a14ed8c452aaced2
Gunnar 'Nick' Bluth <gunnar.bluth@pro-open.de>
2021-11-16 20:21:18 +0100

    refines tests

--- schema/postgresql/t/07-test-func.sql
+++ schema/postgresql/t/07-test-func.sql
@@ -28,5 +28,5 @@ SELECT is(idoreports_get_sla_ok_percent(5,'2020-04-01 12:00', '2020-04-01 16:00'
 
 SELECT is(idoreports_get_sla_ok_percent(6,'2019-04-01 11:43:16','2019-04-01 15:43:16')::float , 46.65972222222222::float,'Host 6 was down until recently');
 
-SELECT is(idoreports_get_sla_ok_percent(7,'2019-04-01 00:00:00','2019-05-01 00:00:00')::float , 100.0::float,'Host 7 had a planned downtime and that did not break the function');
-SELECT is(idoreports_get_sla_ok_percent(7,'2019-04-01 00:00:00','2019-06-01 00:00:00')::float , 100.0::float,'Host 7 had two planned downtime and that did not break the function');
+SELECT is(idoreports_get_sla_ok_percent(7,'2019-04-15 00:00:00','2019-04-15 11:59:00')::float , 100.0::float,'Host 7 had a planned downtime and that did not break the function');
+SELECT is(idoreports_get_sla_ok_percent(7,'2019-04-15 00:00:00','2019-04-16 00:00:00')::float , 100.0::float,'Host 7 had two planned downtime and that did not break the function');
